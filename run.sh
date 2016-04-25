65d56ac18a69417791bc9fb0a3ec5cd5baf37675
Pawel Cejrowski <pcejrowski@gmail.com>
2016-04-25 22:22:25 +0200

    Uruchamianie dockera z Kerasem

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 set -euvx
 
 curdir=`pwd`
-sudo docker run -p 8888:8888 -v ${curdir}:/notebooks -it --rm jakubb/theano-docker         
+sudo docker run -p 8888:8888 -v ${curdir}:/notebooks -it --rm pcej/keras-jupyter-yaafe       
