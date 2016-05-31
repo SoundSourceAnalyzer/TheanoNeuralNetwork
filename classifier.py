e208d02aed6f0c9ed9d4cca1f6146fed89d7809c
humblehound <dejtabejz@gmail.com>
2016-05-31 23:36:53 +0200

    2D data extraction and training WIP

---
+++ classifier.py
@@ -0,0 +1,87 @@
+from keras import backend as K
+from keras.layers.convolutional import Convolution1D
+from keras.layers.core import Dense, Dropout, Activation
+from keras.layers.core import Lambda
+from keras.layers.embeddings import Embedding
+from keras.layers.recurrent import LSTM
+from keras.models import Sequential
+from keras.utils.visualize_util import plot
+from keras.utils import np_utils
+from six.moves import cPickle as pickle
+import numpy
+
+def max_1d(X):
+    return K.max(X, axis=1)
+
+with open('data/input.pickle', 'rb') as f:
+    save = pickle.load(f)
+    train_features = save['train_data']
+    test_features = save['test_data']
+    test_labels = save['test_labels']
+    train_labels = save['train_labels']
+    del save  # gc
+
+test_labels = np_utils.to_categorical(test_labels, 6)
+train_labels = np_utils.to_categorical(train_labels, 6)
+
+# set parameters:
+max_features = 5000
+maxlen = 169
+batch_size = 16
+embedding_dims = 169
+nb_filter = 32
+filter_length = 8
+hidden_dims = 64
+nb_epoch = 15
+
+print(len(train_features), 'train sequences')
+print(len(test_features), 'test sequences')
+
+print('Build model...')
+
+nn = Sequential()
+
+nn.add(LSTM(32, input_shape=(10, 64)))
+
+nn.compile(loss='binary_crossentropy',
+           optimizer='adam',
+           metrics=['accuracy'])
+# wtf= numpy.array(train_features[0])
+numpy.vstack(train_features[0])
+# train_features =
+train_labels = numpy.asarray(train_features[0])
+
+# nn.fit(train_features, train_labels, batch_size=batch_size, nb_epoch=nb_epoch)
+
+# plot(nn, to_file='output.png', show_shapes='true')
+
+#
+# nn.add(Embedding(max_features, embedding_dims, input_length=maxlen, dropout=0.2))
+# nn.add(Convolution1D(nb_filter=32,
+#                      filter_length=5,
+#                      border_mode='same',
+#                      activation='relu',
+#                      subsample_length=1))
+#
+# nn.add(Convolution1D(nb_filter=32,
+#                      filter_length=3,
+#                      border_mode='valid',
+#                      activation='relu',
+#                      subsample_length=1))
+#
+#
+# nn.add(Convolution1D(nb_filter=32,
+#                      filter_length=3,
+#                      border_mode='valid',
+#                      activation='relu',
+#                      subsample_length=1))
+#
+# nn.add(Lambda(max_1d, output_shape=(nb_filter,)))
+#
+# nn.add(Dense(hidden_dims))
+# nn.add(Dropout(0.2))
+# nn.add(Activation('relu'))
+#
+# nn.add(Dense(6))
+# nn.add(Activation('relu'))
+
---
+++ extraction.py
@@ -0,0 +1,73 @@
+from __future__ import print_function
+
+import csv
+import numpy as np
+from six.moves import cPickle as pickle
+
+np.random.seed(1337)  # for reproducibility
+
+
+def load_features(filename):
+    with open(filename, 'rb') as csvfile:
+        next(csvfile)  # skip header
+        data_features = []
+        data_labels = []
+        reader = csv.reader(csvfile)
+        for row in reader:
+            track_feature_array = [row[0], row[1], row[2], row[3:36], row[37], row[38:71], row[72], row[73], row[74],
+                                   row[75], row[76], row[77:100], row[101], row[102:125], row[126], row[127:146],
+                                   row[167:190]]
+            for index, arr in enumerate(track_feature_array):
+                if isinstance(arr, list):
+                    track_feature_array[index] = map(float, arr)
+                else:
+                    track_feature_array[index] = float(arr)
+
+                    data_features.append(track_feature_array)  # features
+                    data_labels.append(row[-1])  # genre
+
+        return data_features, data_labels
+
+
+def persist_extracted_features(train_features, test_features, train_labels, test_labels):
+    try:
+        f = open('data/input.pickle', 'wb')
+        save = {
+            'train_data': train_features,
+            'test_data': test_features,
+            'test_labels': test_labels,
+            'train_labels': train_labels
+        }
+        pickle.dump(save, f, pickle.HIGHEST_PROTOCOL)
+        f.close()
+    except Exception as e:
+        print('Unable to save data to', 'data/input.pickle', ':', e)
+        raise
+
+
+def evens(dataset):
+    return dataset[::2]
+
+
+def odds(dataset):
+    return dataset[1::2]
+
+
+def map_classes(labels, label_map):
+    mappings_rev = {v: k for k, v in label_map.items()}
+    return map(lambda x: mappings_rev[x], labels)
+
+
+train_data_file = 'data/genresTrain.csv'
+test_data_file = 'data/genresTest.csv'
+
+train_features, train_labels = load_features(train_data_file)
+mappings = dict(enumerate(set(train_labels)))
+train_label_mapping = map_classes(train_labels, mappings)
+
+test_features = evens(train_features)
+train_features = odds(train_features)
+train_labels = odds(train_label_mapping)
+test_labels = evens(train_label_mapping)
+
+persist_extracted_features(train_features, test_features, train_labels, test_labels)
